84254fb39837b20ff56ae161a2925afbfa861acb
Matías Noé <97198067+Matii-Noe@users.noreply.github.com>
2023-01-27 14:22:52 -0300

    App terminada

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { DeseosService } from '../services/deseos.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,45 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor( 
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertCtrl: AlertController
+    ) {}
 
+    async agregarLista() {
+      const alert = await this.alertCtrl.create({
+        header:'Nueva Lista',
+        inputs:[{
+          name:'titulo',
+          type:'text',
+          placeholder:'Nombre de la lista'
+        }],
+        buttons:[
+          {
+            text:'Cancelar',
+            role:'cancel',
+            handler: () => {
+              console.log('Cancelar');
+              
+            }
+          },
+          {
+            text:'Crear',
+            handler: ( data ) => {
+              if(data.titulo.length === 0) {
+                return;
+              }
+
+              const listaId = this.deseosService.crearLista( data.titulo );
+
+              //Si no está vacío creo la lista
+              this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+            }
+          }
+        ]
+      });
+
+      alert.present();
+    };
 }
